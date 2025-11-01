d244ee247f0a7a489a5f9830fe7e1b3088b0ab13
HosseinHabibi-fullstack <abyaneh.hh24@gmail.com>
2025-11-01 21:18:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// ===== Hamburger Menu Function =====
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+
+  // opțional — închide meniul când se apasă un link
+  document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navLinks.classList.remove("active");
+    });
+  });
+});
